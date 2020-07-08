738fdebc9999aa2628946ccdb7913b6dfda4b395
kevinkellyyyy <kellyisyanta@gmail.com>
2020-07-08 09:19:24 +0700

    Updates

---
+++ precache-manifest.6a4e0396f1fd394efaffa03b2962914e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d6350189f6baa343c918b0e0ff5f78",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/index.html"
+  },
+  {
+    "revision": "9f2b91fc703d09cf4fd1",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf6e450bf989b1116be0",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/static/js/2.1e0ed143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/static/js/2.1e0ed143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2b91fc703d09cf4fd1",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/static/js/main.9430471c.chunk.js"
+  },
+  {
+    "revision": "c71f39dc9b5c46b6c5bb",
+    "url": "/htt[://kevinkellyyyy.github.io/kevinkellyisyanta/static/js/runtime-main.05e75802.js"
+  }
+]);
